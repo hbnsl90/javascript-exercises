22c5a124e27dc3cb61533b7118b23c0f92400c6a
Harsh Bansal <hbnsl890@gmail.com>
2023-07-15 19:51:49 +0530

    Use the simple loop-- Remember eith post-decrement in the loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(oldString) {
+    let reversedWord = "";
+    let end = oldString.length;
+    while(end--){
+        reversedWord += oldString.at(end);
+    }
+    return reversedWord;
 };
 
 // Do not edit below this line

0334f9ee01f6188f653dd15ebac10706561c542f
Harsh Bansal <hbnsl890@gmail.com>
2023-07-18 15:17:28 +0530

    --> I followed 3 methods --> Learnt about the rest operator, the aguments operations in function scope, ---> Learnt about Array methods such as slice, includes, from, filter, forEach, push etc

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...remove) {
+    const arr = Array.from(arguments[0]);
+for(let a of remove){
+   let idx = arr.findIndex((element)=> element ===a);
+   if(idx!=-1) arr.splice(idx,1);
+}
+return arr;
 };
 
+// Method 2:
+// const removeFromArray = function(array, ...remove) {
+//     const newArray = [];
+//     array.forEach((item)=>{
+//         if(!remove.includes(item)) newArray.push(item);
+//     }
+//     );
+//     return newArray;
+// }
+
+// // Method 3:
+// const removeFromArray = function( array, ...Removelements){
+//     return array.filter(element=> !Removelements.includes(element));
+// }
 // Do not edit below this line
 module.exports = removeFromArray;
